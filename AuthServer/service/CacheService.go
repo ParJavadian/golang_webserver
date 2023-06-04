2ae7677268fa6d1636cf8822c8b4f5b4da8dd98d
Negar <babashahNegar@gmail.com>
2023-06-04 21:53:02 +0330

    Add logs and error handling

--- AuthServer/service/CacheService.go
+++ AuthServer/service/CacheService.go
@@ -19,7 +19,7 @@ func startRedisConnection() {
 	initialized = true
 }
 
-func getValue(ctx context.Context, key string) string {
+func getValue(ctx context.Context, key string) (string, error) {
 	if !initialized {
 		startRedisConnection()
 	}
@@ -28,10 +28,10 @@ func getValue(ctx context.Context, key string) string {
 	}
 	val, err := client.Get(ctx, key).Result()
 	if err != nil {
-		fmt.Println(err)
-		return ""
+		fmt.Println("Error while getting data: ", err)
+		return "", err
 	}
-	return val
+	return val, nil
 }
 
 func cacheData(ctx context.Context, key string, value string, expirationDuration time.Duration) {
@@ -46,7 +46,9 @@ func cacheData(ctx context.Context, key string, value string, expirationDuration
 	}
 	err := client.Set(ctx, key, value, expirationDuration).Err()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error while caching data: ", err)
+	} else {
+		fmt.Println("Successfully cached data with key ", key, " and value ", value)
 	}
 }
 
@@ -59,6 +61,8 @@ func deleteValue(ctx context.Context, key string) {
 	}
 	err := client.Del(ctx, key).Err()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error while deleting data: ", err)
+	} else {
+		fmt.Println("Successfully deleted data with key ", key)
 	}
 }
